627f2941171cc297354e292b3012aea4ec353528
Kenneth Allen <kenneth@kallen.dev>
2023-12-12 17:44:12 +1100

    Solve day 06 part 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,43 +1,68 @@
-use std::iter::zip;
+use std::{iter::zip, num::ParseIntError};
 
 use crate::util::usize;
 
-use nom::{IResult, character::complete::{char, line_ending}, bytes::complete::tag, multi::many1, sequence::{delimited, preceded, terminated, pair}, combinator::eof, Parser};
+use nom::{IResult, character::complete::{char, line_ending, digit1}, bytes::complete::tag, multi::many1, sequence::{delimited, preceded, terminated, pair}, combinator::{eof, verify, map_res, map}};
 
-pub fn part1(file: String) -> usize {
-  let (_, races) = parse(file.as_str()).unwrap();
+pub fn part1(file: String) -> usize { process(file, parse_num_list) }
+pub fn part2(file: String) -> usize { process(file, parse_big_num) }
+
+fn process(file: String, parse_nums: impl Fn(&str) -> IResult<&str, Vec<usize>>) -> usize {
+  let (_, races) = parse(file.as_str(), parse_nums).unwrap();
   races.into_iter()
     .map(|(time, record)| {
-      let mut ways = 0;
-      for hold in 0..=time {
-        if record < hold * (time - hold) {
-          ways += 1
+      let mut lo = 0;
+      let mut hi = time/2;
+      while hi > lo {
+        let mid = (hi + lo) / 2;
+        let score = mid * (time - mid);
+        if score > record {
+          hi = mid;
+        } else {
+          lo = mid + 1;
         }
       }
-      ways
+      time + 1 - 2 * lo
     })
     .product()
 }
 
-fn parse(input: &str) -> IResult<&str, Vec<(usize, usize)>> {
-  terminated(
-    pair(
-      delimited(
-        tag("Time:"),
-        many1(preceded(many1(char(' ')), usize)),
-        line_ending,
+fn parse(input: &str, parse_nums: impl Fn(&str) -> IResult<&str, Vec<usize>>) -> IResult<&str, Vec<(usize, usize)>> {
+  map(
+    verify(
+      terminated(
+        pair(
+          delimited(
+            tag("Time:"),
+            &parse_nums,
+            line_ending,
+          ),
+          delimited(
+            tag("Distance:"),
+            &parse_nums,
+            line_ending,
+          ),
+        ),
+        eof,
       ),
-      delimited(
-        tag("Distance:"),
-        many1(preceded(many1(char(' ')), usize)),
-        line_ending,
-      ),
+      |(times, dists)| times.len() == dists.len(),
     ),
-    eof,
-  ).map(|(times, dists)| {
-    assert!(times.len() == dists.len());
-    zip(times, dists).collect()
-  }).parse(input)
+    |(times, dists)| zip(times, dists).collect(),
+  )(input)
+}
+
+fn parse_num_list(input: &str) -> IResult<&str, Vec<usize>> {
+  many1(preceded(many1(char(' ')), usize))(input)
+}
+
+fn parse_big_num(input: &str) -> IResult<&str, Vec<usize>> {
+  map_res(
+    many1(preceded(many1(char(' ')), digit1)),
+    |chunks| -> Result<_, ParseIntError> {
+      let chunks: String = chunks.into_iter().collect();
+      Ok(vec![chunks.parse()?])
+    },
+  )(input)
 }
 
 #[cfg(test)]
@@ -53,5 +78,15 @@ mod tests {
   #[test]
   fn test1() {
     assert_eq!(part1(sample_file("06")), 2374848);
+  }
+
+  #[test]
+  fn test2_sample() {
+    assert_eq!(part2(sample_file("06a")), 71503);
+  }
+
+  #[test]
+  fn test2() {
+    assert_eq!(part2(sample_file("06")), 39132886);
   }
 }
