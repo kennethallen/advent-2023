ccbc2f3a2d3a24dd74aba0ca697953c7c9216fe8
Kenneth Allen <kenneth@kallen.dev>
2023-12-15 23:30:48 +1100

    Solve day 12 part 1

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::cmp::min;
 
 use crate::util::usize;
 
@@ -11,86 +11,46 @@ pub fn part1(lines: impl Iterator<Item=String>) -> usize {
     .sum()
 }
 
-fn count_solutions_0(mut pat: &[Option<bool>], mut runs: &[usize]) -> Option<usize> {
-  // Strip front
+fn count_solutions_0(pat: &[Option<bool>], runs: &[usize]) -> Option<usize> {
+  let x = count_solutions(pat, runs);
+  println!("{:?} {:?} {:?}", x.unwrap_or_default(), pat.iter().map(|&sym| match sym { None => '?', Some(true) => '#', Some(false) => '.' }).collect::<String>(), runs);
+  //dbg!(x);
+  x
+}
+fn count_solutions(mut pat: &[Option<bool>], mut runs: &[usize]) -> Option<usize> {
   loop {
     match pat.first().copied() {
-      None => return Some(1),
+      None => return if runs.is_empty() { Some(1) } else { None },
       Some(Some(false)) => pat = &pat[1..],
-      Some(None) => break,
       Some(Some(true)) => {
-        let run_len = pat.iter().take_while(|&&sym| sym == Some(true)).count();
-        if run_len == 0 { break; }
-        match run_len.cmp(runs.first()?) {
-          Ordering::Less => {
-            let mut new_runs = runs.to_vec();
-            new_runs[0] -= run_len;
-            return count_solutions_0(&pat[run_len..], &new_runs);
-          }
-          Ordering::Equal => {
-            pat = &pat[run_len..];
-            runs = &runs[1..];
-          },
-          Ordering::Greater => return None, // Observed run longer than specified run
-        }
+        if !could_start_run(pat, runs) { return None; }
+        pat = &pat[min(runs[0]+1, pat.len())..];
+        runs = &runs[1..];
+      }
+      Some(None) => if could_start_run(pat, runs) {
+        return Some(
+          count_solutions_0(&pat[1..], runs).unwrap_or_default()
+          + count_solutions_0(&pat[min(runs[0]+1, pat.len())..], &runs[1..]).unwrap_or_default()
+        );
+      } else {
+        // Assume empty
+        pat = &pat[1..];
       }
     }
   }
-  count_solutions_1(pat, runs)
 }
 
-fn count_solutions_1(mut pat: &[Option<bool>], mut runs: &[usize]) -> Option<usize> {
-  // Starts with ?
-  // Strip back
-  loop {
-    match pat.last().copied() {
-      None => unreachable!("Should always have at least a ? at the beginning"),
-      Some(Some(false)) => pat = &pat[..pat.len()-1],
-      Some(None) => break,
-      Some(Some(true)) => {
-        let run_len = pat.iter().rev().take_while(|&&sym| sym == Some(true)).count();
-        if run_len == 0 { break; }
-        match run_len.cmp(runs.last()?) {
-          Ordering::Less => {
-            let mut new_runs = runs.to_vec();
-            new_runs[runs.len()-1] -= run_len;
-            return count_solutions_1(&pat[..pat.len()-run_len], &new_runs);
-          }
-          Ordering::Equal => {
-            pat = &pat[..pat.len()-run_len];
-            runs = &runs[..runs.len()-1];
-          },
-          Ordering::Greater => return None, // Observed run longer than specified run
-        }
-      }
-    }
-  }
-  count_solutions_2(pat, runs)
-}
+// Test if the first run[0]+1 symbols can satisfy the first run.
+// Precondition: first pattern symbol known not to be empty.
+fn could_start_run(pat: &[Option<bool>], runs: &[usize]) -> bool {
+  if let Some(&rem_range) = runs.first() {
+    rem_range <= pat.len()
+    && !pat[1..rem_range].iter().any(|&sym| sym == Some(false))
 
-fn count_solutions_2(pat: &[Option<bool>], runs: &[usize]) -> Option<usize> {
-  // Starts and ends with ?
-  //if pat.len() == runs.iter().sum::<usize>() + runs.len() - 1 { return 1; } // Minimum
-  if runs.is_empty() {
-    assert!(pat.iter().all(|&sym| sym != Some(true)));
-    return 1;
-  }
-  if runs.len() == 1 {
-    assert!(runs[0] <= pat.len());
-    if runs[0] == pat.len() { return 1; }
-  }
-
-  //panic!("Can't deduce {:?} {:?}", pat, runs);
-  count_solutions_0(&pat[1..], runs) + { // Suppose empty
-    // Suppose full
-    let run_len = runs[0];
-    assert!(run_len < pat.len(), "Huh {:?} {:?}", pat, runs);
-    if pat[1..run_len].iter().all(|&sym| sym != Some(false))
-        && pat[run_len] != Some(true) {
-      count_solutions_1(&pat[1..], &runs[1..])
-    } else {
-      0
-    }
+    // Must be followed by an empty or a ? (presumed empty) or the end of the pattern
+    && pat.get(rem_range).copied() != Some(Some(true))
+  } else {
+    false
   }
 }
 
@@ -120,7 +80,7 @@ mod tests {
 
   #[test]
   fn test1() {
-    assert_eq!(part1(sample_lines("12")), 1);
+    assert_eq!(part1(sample_lines("12")), 7032);
   }
 
   /*#[test]
