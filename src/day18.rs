76bd79edf317e3543c64047f515ba0ae86402e44
Kenneth Allen <kenneth@kallen.dev>
2023-12-24 15:10:50 +1100

    Attempt naive solution to day 18 part 2

--- src/day18.rs
+++ src/day18.rs
@@ -1,17 +1,18 @@
 
 use std::collections::HashSet;
 
-use nom::{IResult, sequence::{terminated, delimited}, character::complete::one_of, combinator::{eof, map_res, map}, bytes::complete::{tag, take}, character::complete::char};
+use nom::{IResult, sequence::{terminated, pair}, character::complete::one_of, combinator::{eof, map_res, map}, bytes::complete::{tag, take}, character::complete::char};
 
 use crate::util::usize;
 
-pub fn part1(lines: impl Iterator<Item=String>) -> usize { process(lines) }
+pub fn part1(lines: impl Iterator<Item=String>) -> usize { process(lines, 0) }
+pub fn part2(lines: impl Iterator<Item=String>) -> usize { process(lines, 1) }
 
-fn process(lines: impl Iterator<Item=String>) -> usize {
+fn process(lines: impl Iterator<Item=String>, instr_idx: usize) -> usize {
   let mut curs: (isize, isize) = (0, 0);
   let mut trench = HashSet::from([curs]);
   for line in lines {
-    let (dir, run, _) = parse(line.as_str()).unwrap().1;
+    let (dir, run) = parse(line.as_str()).unwrap().1[instr_idx];
     for _ in 0..run {
       match dir {
         Dir::R => curs.1 += 1,
@@ -53,23 +54,30 @@ fn process(lines: impl Iterator<Item=String>) -> usize {
   usize::try_from((maxs.1 - mins.1 + 1)*(maxs.0 - mins.0 + 1)).unwrap() - holes.len()
 }
 
-fn parse(input: &str) -> IResult<&str, (Dir, usize, u32)> {
-  let (input, dir) = terminated(map(one_of("UDLR"), |c| match c { 'U' => Dir::U, 'D' => Dir::D, 'L' => Dir::L, 'R' => Dir::R, _ => unreachable!() }), char(' '))(input)?;
-  let (input, run) = terminated(usize, char(' '))(input)?;
-  let (input, color) = terminated(parse_color, eof)(input)?;
-  Ok((input, (dir, run, color)))
-}
-fn parse_color(input: &str) -> IResult<&str, u32> {
-  delimited(
-    tag("(#"),
-    map_res(
-      take(6usize),
-      |s| u32::from_str_radix(s, 16),
+fn parse(input: &str) -> IResult<&str, [(Dir, usize); 2]> {
+  let (input, dir0) = terminated(
+    map(
+      one_of("UDLR"),
+      |c| match c { 'U' => Dir::U, 'D' => Dir::D, 'L' => Dir::L, 'R' => Dir::R, _ => unreachable!() },
     ),
-    char(')'),
-  )(input)
+    char(' '),
+  )(input)?;
+  let (input, run0) = terminated(usize, tag(" (#"))(input)?;
+  let (input, run1) = map_res(take(5usize), |s| usize::from_str_radix(s, 16))(input)?;
+  let (input, dir1) = terminated(
+    map(
+      one_of("0123"),
+      |c| match c { '0' => Dir::R, '1' => Dir::D, '2' => Dir::L, '3' => Dir::U, _ => unreachable!() },
+    ),
+    pair(
+      char(')'),
+      eof,
+    ),
+  )(input)?;
+  Ok((input, [(dir0, run0), (dir1, run1)]))
 }
 
+#[derive(Clone, Copy)]
 enum Dir { R, U, L, D }
 
 #[cfg(test)]
@@ -85,5 +93,15 @@ mod tests {
   #[test]
   fn test1() {
     assert_eq!(part1(sample_lines("18")), 58550);
+  }
+
+  #[test]
+  fn test2_sample() {
+    assert_eq!(part2(sample_lines("18a")), 952408144115);
+  }
+
+  #[test]
+  fn test2() {
+    assert_eq!(part2(sample_lines("18")), 1165);
   }
 }
